b657b2ec84bc38093bef8f8dfd0eafdd05556ad2
Alison Clarke <a.clarke@wellcome.org>
2022-12-11 12:11:34 +0000

    Update setup_day to create input files if they don't exist but .py does

--- setup_day.py
+++ setup_day.py
@@ -5,20 +5,26 @@ day = int(sys.argv[1])
 
 python_file = f"day{day:02}.py"
 if os.path.exists(python_file):
-    print(f"File {python_file} exists")
-    sys.exit(1)
-
-with open(python_file, "w") as f:
-    f.write(
-        f"""import sys
+    print(
+        f"File {python_file} exists; not overwriting (but will create input files if they do not exist)"
+    )
+else:
+    with open(python_file, "w") as f:
+        f.write(
+            f"""import sys
 
 import utils
 
 test_mode = len(sys.argv) > 1
 input_file = f'day{day:02}_test_input.txt' if test_mode else f'day{day:02}_input.txt'
-data = utils.input_as_ints(input_file)
+data = utils.input_as_lines(input_file)
 """
-    )
+        )
 
-open(f"day{day:02}_input.txt", "w").close()
-open(f"day{day:02}_test_input.txt", "w").close()
+input_file = f"day{day:02}_input.txt"
+if not os.path.exists(input_file):
+    open(input_file, "w").close()
+
+test_input_file = f"day{day:02}_input.txt"
+if not os.path.exists(test_input_file):
+    open(test_input_file, "w").close()
