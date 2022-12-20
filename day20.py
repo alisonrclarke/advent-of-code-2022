c584c75ba50eaba5446a9c069bccc3efae2a558b
Alison Clarke <alisonrclarke@gmail.com>
2022-12-20 20:20:52 +0000

    Day 20 part 2
    Using the right value for modulo made things easier

--- day20.py
+++ day20.py
@@ -12,34 +12,46 @@ data = utils.input_as_ints(input_file)
 orig = list(enumerate(data))
 mixed = copy.deepcopy(orig)
 
-s = len(data)
+s = len(data) - 1
 
 for j, n in orig:
     i = mixed.index((j, n))
-    new_i = (i + n)
+    new_i = (i + n) % s
     
-    # correct for wrapping - add or subtract 1 every time we go circle round
-    if n != 0 and new_i <= 0:
-        if new_i // s < 0:
-            new_i += new_i // s
-        else:
-            new_i -= 1
-        new_i = new_i % s
-    elif n != 0 and new_i >= s:
-        if new_i // s > 0:
-            new_i += (new_i // s)
-        else:
-            new_i += 1
-        new_i = new_i % s
-
     mixed.pop(i)
     mixed.insert(new_i, (j, n))
 
     # print([m[1] for m in mixed])
    
 i0 = mixed.index((data.index(0), 0))
-_, n1000 = mixed[(i0+1000)%s]
-_, n2000 = mixed[(i0+2000)%s]
-_, n3000 = mixed[(i0+3000)%s]
+_, n1000 = mixed[(i0+1000)%(s+1)]
+_, n2000 = mixed[(i0+2000)%(s+1)]
+_, n3000 = mixed[(i0+3000)%(s+1)]
 print("Coordinates:", n1000, n2000, n3000)
 print("Part1: ", n1000 + n2000 + n3000)
+
+# Part 2
+decryption_key = 811589153
+new_data = [(i, n*decryption_key) for i, n in orig]
+mixed = copy.deepcopy(new_data)
+
+# s = s-1
+
+# print([m[1] for m in mixed])
+
+for _ in range(10):
+    for j, n in new_data:
+        i = mixed.index((j, n))
+        new_i = (i + n) % s
+        
+        mixed.pop(i)
+        mixed.insert(new_i, (j, n))
+
+    # print([m[1] for m in mixed])
+   
+i0 = mixed.index((data.index(0), 0))
+_, n1000 = mixed[(i0+1000)%(s+1)]
+_, n2000 = mixed[(i0+2000)%(s+1)]
+_, n3000 = mixed[(i0+3000)%(s+1)]
+print("Coordinates:", n1000, n2000, n3000)
+print("Part2: ", n1000 + n2000 + n3000)
